086c58aff7a116de518234322132444440549071
mush42 <musharraf.omer@gmail.com>
2021-01-03 13:55:47 +0200

    first steps to support posix/linux platform

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from pathlib import Path
 from setuptools import setup, find_packages
 from bookworm import app
@@ -19,19 +21,41 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/mush42/bookworm/",
+    download_url=app.website,
     license="MIT",
     packages=find_packages(exclude=["tests"]),
-    platforms=["Windows"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={"gui_scripts": ["bookworm=bookworm.__main__:main"]},
+    entry_points={
+        "gui_scripts": ["bookworm=bookworm.__main__:main"]
+    },
     install_requires=REQUIREMENTS,
+    platforms=["Windows", "Linux"],
+    keywords=[
+        "reader",
+        "eBook",
+        "accessibility",
+        "a11y",
+        "blind",
+        "pdf",
+        "epub",
+        "tts",
+    ],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Desktop Environment",
         "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
-        "Operating System ::Windows",
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: Microsoft :: Windows :: Windows 8",
+        "Operating System :: Microsoft :: Windows :: Windows 8.1",
+        "Operating System :: Microsoft :: Windows :: Windows 10 ",
+        "Operating System :: POSIX :: Linux ",
         "Programming Language :: Python :: 3.7",
+        "Topic :: Adaptive Technologies",
+        "Topic :: Desktop Environment :: Gnome",
+        "Topic :: Education",
     ],
 )
