0ae702e72f82ef776baa0e8da6d51dee40f029aa
mush42 <musharraf.omer@gmail.com>
2021-04-28 21:01:15 +0200

    Minor bug fix.

--- bookworm/structured_text/primitives.py
+++ bookworm/structured_text/primitives.py
@@ -30,7 +30,7 @@ class TextRange(Container):
     def __repr__(self):
         return f"TextRange(start={self.start}, stop={self.stop})"
 
-    @cached_property
+    @property
     def midrange(self):
         return math.floor((self.start + self.stop)/2)
 
