eaa86269bdca3ef93d02e266c0976d41c9a29333
mush42 <musharraf.omer@gmail.com>
2022-02-05 23:23:29 +0200

    WIP: tested with tesseract 5.1 and pytesseract.

--- bookworm/ocr_engines/tesseract_ocr_engine/pyocr/util.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-def digits_only(string):
-    """Return all digits that the given string starts with."""
-    match = re.match(r"\D*(?P<digits>\d+)", string)
-    if match:
-        return int(match.group("digits"))
-    return 0
