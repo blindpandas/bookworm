941476d4ade506d044fe38383b305df4da848c09
mush42 <musharraf.omer@gmail.com>
2022-02-18 20:48:12 +0200

    No need to cache docbook to html results, the parsing is very fast. Completed the build part of this feature.

--- bookworm/document/formats/docbook.py
+++ bookworm/document/formats/docbook.py
@@ -9,10 +9,9 @@ import dateparser
 import lxml
 from functools import cached_property
 from pathlib import Path
-from diskcache import Cache
 from more_itertools import flatten, first as get_first_element
 from bookworm import app
-from bookworm.paths import home_data_path, resources_path
+from bookworm.paths import resources_path
 from bookworm.i18n import LocaleInfo
 from bookworm.logger import logger
 from .. import (
@@ -43,7 +42,7 @@ class DocbookDocument(BaseHtmlDocument):
 
     @cached_property
     def language(self):
-        if (lang_tag := self.xml_tree.attrib.get("lang")):
+        if lang_tag := self.xml_tree.attrib.get("lang"):
             try:
                 return LocaleInfo(lang_tag)
             except ValueError:
@@ -59,22 +58,21 @@ class DocbookDocument(BaseHtmlDocument):
         xml_tree = self.xml_tree
         if not (title := xml_tree.xpath("/book/title//text()")):
             title = xml_tree.xpath("/book/bookinfo/title//text()")
-        title = get_first_element(
-            title,
-            Path(self.get_file_system_path()).stem
-        )
+        title = get_first_element(title, Path(self.get_file_system_path()).stem)
         author_firstname = get_first_element(
-            xml_tree.xpath("/book/bookinfo/author/firstname//text()"),
-            ""
+            xml_tree.xpath("/book/bookinfo/author/firstname//text()"), ""
         )
         author_surname = get_first_element(
-            xml_tree.xpath("/book/bookinfo/author/surname//text()"),
-            ""
+            xml_tree.xpath("/book/bookinfo/author/surname//text()"), ""
         )
-        author = " ".join([author_firstname, author_surname,])
+        author = " ".join(
+            [
+                author_firstname,
+                author_surname,
+            ]
+        )
         publisher = get_first_element(
-            xml_tree.xpath("/book/bookinfo/corpname//text()"),
-            ""
+            xml_tree.xpath("/book/bookinfo/corpname//text()"), ""
         )
         creation_date = xml_tree.xpath("/book/bookinfo/date//text()")
         if creation_date:
@@ -82,7 +80,7 @@ class DocbookDocument(BaseHtmlDocument):
                 creation_date[0],
                 languages=[
                     self.language.two_letter_language_code,
-                ]
+                ],
             )
             creation_date = self.language.format_datetime(
                 parsed_date, date_only=True, format="long", localized=True
@@ -94,20 +92,10 @@ class DocbookDocument(BaseHtmlDocument):
             publisher=publisher,
         )
 
-    def _get_cache_directory(self):
-        return str(home_data_path(".docbook_html_cache"))
-
     def get_html(self):
         if (html_string := getattr(self, "html_string", None)) is not None:
             return html_string
-        cache_key = self.uri.to_uri_string()
-        _cache = Cache(
-            self._get_cache_directory(), eviction_policy="least-frequently-used"
-        )
-        if not (html_content := _cache.get(cache_key)):
-            html_content = self._get_html_from_docbook()
-            _cache.set(cache_key, html_content, expire=EXPIRE_TIMEOUT)
-        return html_content
+        return self._get_html_from_docbook()
 
     def parse_html(self):
         return self.parse_to_full_text()
@@ -116,17 +104,15 @@ class DocbookDocument(BaseHtmlDocument):
         xslt = lxml.etree.parse(os.fspath(self._get_xslt_resource_path()))
         transform = lxml.etree.XSLT(xslt)
         content_tree = transform(copy.copy(self.xml_tree))
-        block_classes = [
-            "chapter",
-            *[f"sect{i}" for i in range(1, 7)]
-        ]
+        block_classes = ["chapter", *[f"sect{i}" for i in range(1, 7)]]
         span_block_elements = flatten(
-            content_tree.xpath(f"//*[@class='{blk_cls}']")
-            for blk_cls in block_classes
+            content_tree.xpath(f"//*[@class='{blk_cls}']") for blk_cls in block_classes
         )
         for elem in span_block_elements:
             elem.tag = "p"
-        for el in content_tree.xpath('//ns:sup',namespaces={'ns':'http://www.w3.org/1999/xhtml'}):
+        for el in content_tree.xpath(
+            "//ns:sup", namespaces={"ns": "http://www.w3.org/1999/xhtml"}
+        ):
             el.clear()
         return lxml.etree.tostring(content_tree, encoding="unicode")
 
