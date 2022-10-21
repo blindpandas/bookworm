0791f47e89699f9f0232d13ed8ce4a2938b39157
mush42 <musharraf.omer@gmail.com>
2022-10-21 21:55:18 +0200

    WIP

--- bookworm/platforms/win32/speech_engines/sapi/sp_engine.py
+++ bookworm/platforms/win32/speech_engines/sapi/sp_engine.py
@@ -1,119 +1,193 @@
 # coding: utf-8
 
-from contextlib import suppress
-
+import clr
 import System
 from System.Globalization import CultureInfo
-
-from bookworm.i18n import LocaleInfo
-from bookworm.logger import logger
 from bookworm.platforms.win32.runtime import reference_gac_assembly
+
+reference_gac_assembly("System.Speech\*\System.Speech.dll")
+from System.Speech import Synthesis
+from .sp_utterance import SapiSpeechUtterance
+
+
+
+
+from enum import IntEnum
+from collections import OrderedDict
+import comtypes.client
+from comtypes import COMError
+import winreg
+import weakref
+
+from bookworm import typehints as t
+from bookworm.i18n import LocaleInfo
 from bookworm.speechdriver.engine import BaseSpeechEngine, VoiceInfo
 from bookworm.speechdriver.enumerations import EngineEvent, SynthState
 from bookworm.speechdriver.utterance import SpeechStyle
+from bookworm.logger import logger
+
+
 
 log = logger.getChild(__name__)
 
 
-_sapi_available = False
-try:
-    reference_gac_assembly("System.Speech\*\System.Speech.dll")
-    from System.Speech import Synthesis
+class SPAudioState(IntEnum):
+    # https://docs.microsoft.com/en-us/previous-versions/windows/desktop/ms720596(v=vs.85)
+    Closed = 0
+    Stopped = 1
+    Paused = 2
+    Running = 3
 
-    from .sp_utterance import SapiSpeechUtterance
 
-    _sapi_available = True
-except OSError as e:
-    log.error(f"Could not load Sapi5 speech engine: {e}")
+class SpeechVoiceSpeakFlags(IntEnum):
+    # https://docs.microsoft.com/en-us/previous-versions/windows/desktop/ms720892(v=vs.85)
+    Async = 1
+    PurgeBeforeSpeak = 2
+    IsXML = 8
+
+
+class SpeechVoiceEvents(IntEnum):
+    # https://msdn.microsoft.com/en-us/previous-versions/windows/desktop/ms720886(v=vs.85)
+    StartInputStream = 2
+    EndInputStream = 4
+    Bookmark = 16
+
+
+class SapiEventSink(object):
+    """Handles SAPI event notifications.
+    See https://msdn.microsoft.com/en-us/library/ms723587(v=vs.85).aspx
+    """
+
+    def __init__(self, synthRef: weakref.ReferenceType):
+        self.synthRef = synthRef
+
+    def StartStream(self, streamNum, pos):
+        synth = self.synthRef()
+        if synth is None:
+            log.warning(
+                "Called StartStream method on SapiSink while the synthesizer is dead"
+            )
+        else:
+            for handler in synth.event_handlers.get(EngineEvent.state_changed, ()):
+                handler(SynthState.busy)
+
+    def Bookmark(self, streamNum, pos, bookmark, bookmarkId):
+        synth = self.synthRef()
+        if synth is None:
+            log.warning(
+                "Called Bookmark method on SapiSink while the synthesizer is dead"
+            )
+        else:
+            for handler in synth.event_handlers.get(EngineEvent.bookmark_reached, ()):
+                handler(bookmark)
+
+    def EndStream(self, streamNum, pos):
+        synth = self.synthRef()
+        if synth is None:
+            log.warning("Called stream end method on EndStream while the synthesizer is dead")
+        else:
+            for handler in synth.event_handlers.get(EngineEvent.state_changed, ()):
+                handler(SynthState.ready)
+
 
 
 class SapiSpeechEngine(BaseSpeechEngine):
-    """Our Pythonic Interface to SAPI speech engine."""
+    name = "sapi5"
+    display_name = "Microsoft Speech API version 5 (SAPI 5}"
+    default_rate = 50
+    default_volume = 75
+    COM_CLASS: t.ClassVar = "SAPI.SPVoice"
 
-    name = "sapi"
-    display_name = _("Microsoft Speech API Version 5")
+    @classmethod
+    def check(cls):
+        try:
+            r = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, cls.COM_CLASS)
+            r.Close()
+            return True
+        except:
+            return False
 
     def __init__(self):
         super().__init__()
-        self.synth = Synthesis.SpeechSynthesizer()
-        self.synth.SetOutputToDefaultAudioDevice()
-        self.synth.BookmarkReached += self._on_bookmark_reached
-        self.synth.StateChanged += self._on_state_changed
-        self.__event_handlers = {}
-
-    @classmethod
-    def check(self):
-        return _sapi_available
+        self.event_handlers = {}
+        self._tts_audio_stream = None
+        self._pitch = 50
+        self.tts = comtypes.client.CreateObject(self.COM_CLASS)
+        self._events_connection = comtypes.client.GetEvents(
+            self.tts, SapiEventSink(weakref.ref(self))
+        )
+        self.tts.EventInterests = (
+            SpeechVoiceEvents.StartInputStream
+            | SpeechVoiceEvents.Bookmark
+            | SpeechVoiceEvents.EndInputStream
+        )
 
     def close(self):
-        with suppress(System.ObjectDisposedException):
-            self.stop()
-        self.synth.BookmarkReached -= self._on_bookmark_reached
-        self.synth.StateChanged -= self._on_state_changed
-        self.synth.Dispose()
-        self.synth.Finalize()
+        self._events_connection = None
+        self._tts = None
+        super().close()
 
-    def get_voices(self, language=None):
-        rv = []
-        for voice in self.synth.GetInstalledVoices():
-            if not voice.Enabled:
-                continue
-            info = voice.VoiceInfo
-            if (voice_culture := info.Culture) is not None:
-                voice_language = LocaleInfo(voice_culture.IetfLanguageTag)
-            else:
-                log.exception(
-                    f"Failed to obtain culture information for voice {info.Name}"
-                )
-                continue
-            rv.append(
-                VoiceInfo(
-                    id=info.Id,
-                    name=info.Name,
-                    desc=info.Description,
-                    language=voice_language,
-                    gender=info.Gender,
-                    age=info.Age,
-                )
-            )
-        return rv
-
-    @property
-    def state(self):
-        return SynthState(self.synth.State)
+    def get_voices(self):
+        v = self._get_voice_tokens()
+        retval = []
+        for i in range(len(v)):
+            try:
+                id = v[i].Id
+                name = v[i].GetDescription()
+                try:
+                    language = v[i].getattribute("language").split(";")[0]
+                except KeyError:
+                    language = "en"
+            except COMError:
+                log.warning("Could not get the voice info. Skipping...")
+            retval.append(VoiceInfo(
+                id=id,
+                name=name,
+                desc=name,
+                language=LocaleInfo(language)
+            ))
+        return retval
 
     @property
     def voice(self):
-        for voice in self.get_voices():
-            if voice.id == self.synth.Voice.Id:
-                return voice
+        return more_itertools.first_true(
+            voices := self.get_voices(),
+            pred=lambda v: v.id == self.tts.voice.Id,
+            default=voices[0]
+        )
 
     @voice.setter
     def voice(self, value):
-        try:
-            self.synth.SelectVoice(value.name)
-        except System.ArgumentException:
-            raise ValueError(f"Can not set voice to  {value}.")
+        tokens = self.get_voice_token()
+        for i in range(len(tokens)):
+            voice = tokens[i]
+            if value.id == voice.Id:
+                self.tts.voice = voice
+                break
 
     @property
     def rate(self):
-        return (self.synth.Rate * 5) + 50
+        return (self.tts.rate * 5) + 50
 
     @rate.setter
     def rate(self, value):
-        if not (0 <= value <= 100):
-            raise ValueError(f"Value {value} for rate is out of range.")
-        self.synth.Rate = (value - 50) / 5
+        self.tts.Rate = self._percentToRate(value)
 
     @property
     def volume(self):
-        return self.synth.Volume
+        return self.tts.volume
 
     @volume.setter
     def volume(self, value):
-        if not (0 <= value <= 100):
-            raise ValueError(f"Value {value} for volume is out of range.")
-        self.synth.Volume = value
+        self.tts.Volume = value
+
+    @property
+    def pitch(self):
+        return self._pitch
+
+    @pitch.setter 
+    def pitch(self, value):
+        self._pitch = value
 
     def speak_utterance(self, utterance):
         # We need to wrap the whole utterance in another
@@ -125,38 +199,41 @@ class SapiSpeechEngine(BaseSpeechEngine):
         )
         with voice_utterance.set_style(SpeechStyle(voice=self.voice)):
             voice_utterance.append_utterance(utterance)
-        self.synth.SpeakAsync(voice_utterance.prompt)
+        flags = SpeechVoiceSpeakFlags.IsXML | SpeechVoiceSpeakFlags.Async
+        self.tts.Speak(voice_utterance.prompt.ToXml(), flags)
+
+    def stop(self):
+        self.tts.Speak(
+            None, SpeechVoiceSpeakFlags.Async | SpeechVoiceSpeakFlags.PurgeBeforeSpeak
+        )
+
+    def pause(self):
+        self.tts.Pause()
+        for handler in synth.event_handlers.get(EngineEvent.state_changed, ()):
+            handler(SynthState.paused)
+
+    def resume(self):
+        self.tts.Resume()
+        for handler in synth.event_handlers.get(EngineEvent.state_changed, ()):
+            handler(SynthState.busy)
 
     def preprocess_utterance(self, utterance):
         sp_utterance = SapiSpeechUtterance()
         sp_utterance.populate_from_speech_utterance(utterance)
         return sp_utterance
 
-    def stop(self):
-        if self.state is not SynthState.ready:
-            if self.state is SynthState.paused:
-                self.synth.Resume()
-            self.synth.SpeakAsyncCancelAll()
-
-    def pause(self):
-        if self.state is SynthState.busy:
-            self.synth.Pause()
-
-    def resume(self):
-        if self.state is SynthState.paused:
-            self.synth.Resume()
-
     def bind(self, event, handler):
+        """Bind a member of `EngineEvents` enum to a handler."""
         if event not in (EngineEvent.bookmark_reached, EngineEvent.state_changed):
             raise NotImplementedError
-        self.__event_handlers.setdefault(event, []).append(handler)
+        self.event_handlers.setdefault(event, []).append(handler)
 
-    def _on_bookmark_reached(self, sender, args):
-        handlers = self.__event_handlers.get(EngineEvent.bookmark_reached, ())
-        for handler in handlers:
-            handler(self, args.Bookmark)
+    def _get_voice_tokens(self):
+        """Provides a collection of sapi5 voice tokens. Can be overridden by subclasses if tokens should be looked for in some other registry location."""
+        return self.tts.getVoices()
 
-    def _on_state_changed(self, sender, args):
-        handlers = self.__event_handlers.get(EngineEvent.state_changed, ())
-        for handler in handlers:
-            handler(self, SynthState(sender.State))
+    def _percentToRate(self, percent):
+        return (percent - 50) // 5
+
+    def _percentToPitch(self, percent):
+        return percent // 2 - 25
