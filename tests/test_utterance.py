2431b66a0f71417066f84d8f2dd34abbea0dacde
mush42 <musharraf.omer@gmail.com>
2022-11-08 16:07:35 +0200

    Fixed a failing unit test that caused a failure in CI. Now all tests pass!

--- tests/test_utterance.py
+++ tests/test_utterance.py
@@ -1,6 +1,8 @@
 import pytest
 from lxml import etree
+from bookworm.i18n import LocaleInfo
 from bookworm.speechdriver.utterance import SpeechUtterance
+from bookworm.speechdriver.element.converter import ssml_converter
 
 
 def test_utterance_compile_to_ssml():
@@ -10,7 +12,7 @@ def test_utterance_compile_to_ssml():
     ut.add_audio("file:///C:/myaudio.wav")
     with ut.new_paragraph():
         ut.add_sentence("World")
-    ssml_string = ut.compile_to_ssml()
+    ssml_string = ssml_converter.convert(ut, localeinfo=LocaleInfo("en-US"))
     ssml_tree = etree.fromstring(ssml_string)
     assert ssml_tree[0].attrib["name"] == "mark1"
     
