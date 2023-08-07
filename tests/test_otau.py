8fb3177ef2cc547ccc4a5f81610d933bf6843b89
pauliyobo <pauliyobo@gmail.com>
2023-08-07 07:20:36 +0200

    Added tests for otau

---
+++ tests/test_otau.py
@@ -0,0 +1,19 @@
+import pytest
+
+from bookworm.otau import UpdateChannel
+
+
+def test_is_not_valid_identifier():
+    with pytest.raises(TypeError):
+        channel = UpdateChannel('test')
+
+
+def test_is_valid_identifier():
+    valid_identifiers = ('', 'a', 'b', 'dev')
+    for identifier in valid_identifiers:
+        channel = UpdateChannel(identifier)
+
+def test_is_major_version():
+    c = UpdateChannel('')
+    assert c.is_major == True
+
