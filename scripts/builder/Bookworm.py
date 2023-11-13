9a5b6bcd03bebe4a215d713852c84bb197c75c4f
mush42 <musharraf.omer@gmail.com>
2023-11-13 14:04:55 +0200

    Revert "Bookworm is now compiled as a console application, but an ad hoc hack is used to suppress the extra console window"
    This reverts commit 5bfd675a15807058a879b2152e7108578f4b4006.

--- scripts/builder/Bookworm.py
+++ scripts/builder/Bookworm.py
@@ -1,34 +1,18 @@
 # coding: utf-8
 
+
 import os
 import sys
 
-# sys.stdout = open(os.devnull, "w+")
-# sys.stderr = open(os.devnull, "w+")
+sys.stdout = open(os.devnull, "w+")
+sys.stderr = open(os.devnull, "w+")
 
 import multiprocessing
 
 multiprocessing.freeze_support()
 
 
-def suppress_console_window() -> None:
-    """We need to be able to use CLI args even when bookworm is an executable
-    The issue is that when compiling in windowed mode, we have no access to stdin, stdout and stderr, because of how windows, and python behave in these circumstances
-    Specifically, in windowed mode, pythonw.exe is used, rather than python.exe
-    This hack hides the console window when bookworm starts, obtaining the same effect of windowed mode, but retaining the option to use CLI arguments
-    Code taken from: https://stackoverflow.com/questions/67610859/show-stdout-with-pyinstaller-noconsole
-    """
-    import ctypes
-
-    kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
-    process_array = (ctypes.c_uint8 * 1)()
-    num_processes = kernel32.GetConsoleProcessList(process_array, 1)
-    if num_processes < 3:
-        ctypes.WinDLL('user32').ShowWindow(kernel32.GetConsoleWindow(), 0)
-
-
 if __name__ == "__main__":
     from bookworm import bookworm
 
-    suppress_console_window()
     bookworm.main()
