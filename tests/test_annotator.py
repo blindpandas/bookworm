d4e85a3492c6f3e313abd3a00e0247a66cf7f4fc
pauliyobo <pauliyobo@gmail.com>
2025-05-04 20:43:22 +0200

    Add tests covering annotation sorting

--- tests/test_annotator.py
+++ tests/test_annotator.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import pytest
 
 from bookworm.annotation import NoteTaker
@@ -21,3 +23,30 @@ def test_notes_can_not_overlap(asset, reader):
     assert annot.overlaps(None, None, 0, 0)
     # This should not overlap
     assert annot.overlaps(None, None, 0, 2) == False
+
+
+def test_notes_respect_sort_criteria(asset, reader):
+    uri = DocumentUri.from_filename(asset("roman.epub"))
+    reader.load(uri)
+    # the shape of the list's elements is:
+    # (title, content, page_number, position, start_pos, end_pos)
+    notes = [
+        ("first test", "test", 0, 0, 0, 1),
+        ("second test", "test", 0, 1, 1, 2),
+        ("third test", "test", 0, 5, 5, 10),
+        ("fourth test", "test", 0, 11, 11, 15),
+        ("fifth test", "test", 0, 16, 16, 20),
+    ]        
+    expected_titles = [x[0] for x in sorted(notes, key = lambda x: x[3])]
+    annotator = NoteTaker(reader)
+    for note in notes:
+        annotator.create(
+            title=note[0],
+            content=note[1],
+            page_number=note[2],
+            position=note[3],
+            start_pos=note[4],
+            end_pos=note[5],
+        )
+    titles = [x.title for x in annotator.get_all(asc=True)]
+    assert expected_titles == titles
